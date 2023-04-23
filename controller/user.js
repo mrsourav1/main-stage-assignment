dc08d5e2a8a44ec0c83a8792f7ba8786bbdf1ddb
mrsourav1 <99540120+mrsourav1@users.noreply.github.com>
2023-04-23 19:50:36 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -1,49 +1,5 @@
 import User from "../model/user.js"
 
-// export const getUser = async(req,res)=>{
-//     try{
-//         const {search,location,verified} = req.query
-      
-//         const query = {
-//           name: { $regex: search, $options: 'i' },
-//           location: { $regex: location, $options: 'i' },
-//           verified: verified,
-//         };
-//         if(!verified){
-//             res.json("USer is not verified")
-//         }
-//         const users = await User.find(query).sort({name:1})
-        
-//         res.status(200).json(users)
-//     }
-//     catch(err){
-//         console.log(err)
-//     }
-// }
-
-// export const getUser = async (req, res) => {
-//   try {
-//     const { search, location, verified } = req.query;
-//     const locations = location ? location.split(",") : [];
-
-//     const query = {
-//       name: { $regex: search, $options: "i" },
-//       location: { $in: locations },
-//       verified: verified,
-//     };
-
-//     if (!verified) {
-//       res.json("User is not verified");
-//     }
-
-//     const users = await User.find(query).sort({ name: 1 });
-
-//     res.status(200).json(users);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
 export const getUser = async (req, res) => {
     try {
       const { search, locations, verified } = req.query;
