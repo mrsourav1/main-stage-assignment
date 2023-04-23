90c48dc6b9274dd0e9d0abde86c49978aff2c968
mrsourav1 <99540120+mrsourav1@users.noreply.github.com>
2023-04-23 19:51:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,25 +12,6 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// const user1 = new User({
-//     name: 'ALex',
-//     location: 'delhi',
-//     verified: true,
-//   });
-  
-//   const user2 = new User({
-//     name: 'Sam Curran',
-//     location: 'Mumbai',
-//     verified: true,
-//   });
-
-  
-//   Promise.all([user1.save(), user2.save()])
-//     .then(() => console.log('Temporary users added'))
-//     .catch(err => console.error('Could not add temporary users', err));
-  
-
-
 app.get("/",(req,res)=>{
     res.send("hello")
 })
